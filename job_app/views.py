2e8245494f53acc11193bdc5b3580cecc49c01ce
Clifford Hepplethwaite <cliffordrh@gmail.com>
2023-01-21 09:47:33 +0200

    Created basic templates

--- job_app/views.py
+++ job_app/views.py
@@ -2,3 +2,30 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'job_app/index.html',{})
+
+def about(request):
+    return render(request,'job_app/site/about.html',{})
+
+def career_advice(request):
+    return render(request,'job_app/site/career_advice.html',{})
+
+def contact(request):
+    return render(request,'job_app/site/contact.html',{})
+
+def education(request):
+    return render(request,'job_app/site/education.html',{})
+
+def legal(request):
+    return render(request,'job_app/site/legal.html',{})
+
+def payroll(request):
+    return render(request,'job_app/site/payroll.html',{})
+
+def post_job(request):
+    return render(request,'job_app/site/post_job.html',{})
+
+def recruitment(request):
+    return render(request,'job_app/site/recruitment.html',{})
+
+def submit_resume(request):
+    return render(request,'job_app/site/submit_resume.html',{})
